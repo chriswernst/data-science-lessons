14c7cb81c4142d75e662f67993ef551dc19d8a5d
Chris Ernst <chriswernst@gmail.com>
2017-10-26 19:00:41 -0700

    docs: update Python Notes

--- Python_Notes.py
+++ Python_Notes.py
@@ -162,7 +162,40 @@ b2
   Out[228]: array([6, 7, 8])
 
 
-#################### MODULES / LIBRARIES ########################
+#################################### PYTHON KEYWORDS  ##################
+
+# To get a list of keywords:
+import keyword
+print(keyword.kwlist)
+# OUTPUTS
+
+['False', 'None', 'True', 'and', 'as', 'assert', 'break', 'class', 'continue', 'def', 'del', 'elif', 'else', 'except', 'finally', 'for', 'from', 
+'global', 'if', 'import', 'in', 'is', 'lambda', 'nonlocal', 'not', 'or', 'pass', 'raise', 'return', 'try', 'while', 'with', 'yield']
+
+
+############################### BUILT IN FUNCTIONS  ####################################
+
+# To generate a list of functions and errors
+builtIns = dir(__builtins__)
+builtInFunctions =  builtIns[79:]
+errors = builtIns[:70]
+
+
+# Some useful functions in action:
+testList = [1,4,8,9,10,9,4]
+type(testList)
+setTest = set(testList)
+# Gives back just the distinct values
+{1, 4, 8, 9, 10}
+
+subList = [1,9,4,4]
+subSet = set(subList)
+
+# Finding Subsets in Sets
+subSet <= setTest 
+# Will return TRUE
+
+################################ MODULES / LIBRARIES ################################
 
 import numpy
 import scipy
